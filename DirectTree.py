19fff2a1e18d9b9bc405bf62ed2dca8dcd625e0c
Tim Vaughan <tgvaughan@gmail.com>
2015-05-19 09:50:20 +1200

    Working in log space.

--- DirectTree.py
+++ DirectTree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser, FileType
+from sys import stdout
 
 import numpy as np
 from numpy.random import exponential, choice, uniform
@@ -48,12 +49,12 @@ class Lineage:
 
         return sum(map(lambda x: x[0]!=x[1], zip(otherSequence, thisSeq)))
 
-def probHD(h, t, L):
+def logProbHD(h, t, L):
     """Probability of evolving to sequence h away from start in time t."""
     p = 0.75*(1.0-np.e**(-t*4.0/3.0))
 
     #return binom(L,h)*((1-p)**(L-h))*(p**h)
-    return ((1-p)**(L-h))*(p**h)
+    return (L-h)*np.log(1-p) + h*np.log(p)
 
 
 def readFASTA(inFile):
@@ -76,6 +77,8 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(description='Direct tree sampler.')
     parser.add_argument("fasta", type=FileType('r'), help='FASTA file (contemporaneously sampled)')
+    parser.add_argument("--theta", type=float, default=1.0, help='Parameter value.')
+    parser.add_argument("-o", dest="outfile", type=FileType('w'), default=stdout, help='Output file (default stdout)')
 
     args = parser.parse_args()
 
@@ -83,27 +86,28 @@ if __name__ == '__main__':
 
     L = len(seqs.values()[0])
 
-    print "T"
+    args.outfile.write("T\n")
     for s in range(1000):
+        T = args.theta
         lineage0 = Lineage(seqs.values()[0])
-        T = 0.1
         h = lineage0.getHD(T, seqs.values()[1])
-        P = probHD(h, T, L)*np.e**(-T)
+        logP = logProbHD(h, T, L) - T/args.theta
 
-        for iter in range(100):
+        for iter in range(200):
             #Tp = T + uniform(-0.001,0.001)
             f = uniform(0.8, 1.0/0.8)
             Tp = T*f
 
             if Tp<0:
-                alpha = 0
+                alpha = float('-inf')
             else:
                 hp = lineage0.getHD(Tp, seqs.values()[1])
-                Pp = probHD(hp, Tp, L)*np.e**(-Tp)
-                alpha = min(Pp/P/f, 1.0)
+                logPp = logProbHD(hp, Tp, L) - Tp/args.theta
+                logalpha = logPp - logP - np.log(f)
 
-            if alpha == 1 or (alpha > 0 and uniform()<alpha):
+            if logalpha>0  or uniform()<np.e**logalpha:
                 T = Tp
-                P = Pp
+                logP = logPp
 
-        print T
+        args.outfile.write(str(T) + "\n")
+        args.outfile.flush()
